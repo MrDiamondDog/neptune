df3add00cdc37e48fc6c7b8facb99f51bd41890c
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2026-07-27 20:40:05 -0600

    feat: basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Button from "@/components/primitives/Button";
+import Neptune from "@/components/Neptune";
+import Button, { ButtonLooks } from "@/components/primitives/Button";
+import Divider from "@/components/primitives/Divider";
 
 export default function Home() {
 	return (
-		<main className="">
-			<a href="/auth"><Button>Click here nerd</Button></a>
+		<main className="bg-linear-135 from-bg to-bg-lighter w-full h-screen">
+			<div className="absolute-center text-center flex flex-col items-center">
+				<Neptune />
+				<p className="whitespace-pre-wrap">
+					A simple self-hosted app to{"\n"}
+					organize your college life.
+				</p>
+				<Divider />
+				<a href="/app" className="w-full"><Button look={ButtonLooks.CUSTOM} className="bg-linear-330 from-primary to-tertiary">Get Started</Button></a>
+			</div>
 		</main>
 	);
 }
