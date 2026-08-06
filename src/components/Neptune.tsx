3688d1cca099ecff33fd4548a3d2f26b44f8e0c8
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2026-08-05 19:27:39 -0600

    fix: neptune logo on mobile

--- src/components/Neptune.tsx
+++ src/components/Neptune.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
 export default function Neptune() {
-	return <Link className="flex gap-2 md:gap-3 items-center" href="/">
-		<img src="/neptune.png" className="w-6 md:w-8" />
-		<h1 className="text-xl md:text-3xl bg-linear-330 from-[#7d9fe2] to-[#3056a5] bg-clip-text text-transparent">Neptune</h1>
+	return <Link className="flex gap-2 items-center" href="/">
+		<img src="/neptune.png" className="w-8" />
+		<h1 className="text-3xl bg-linear-330 from-[#7d9fe2] to-[#3056a5] bg-clip-text text-transparent hidden md:block">Neptune</h1>
 	</Link>;
 }
