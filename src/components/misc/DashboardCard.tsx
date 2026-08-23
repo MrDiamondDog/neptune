0ca172ea8173de7a3e50d81d0a884a6eec3db223
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2026-08-22 19:23:58 -0600

    fix: dashboardcard overflow

--- src/components/misc/DashboardCard.tsx
+++ src/components/misc/DashboardCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 export function DashboardCard(props: React.HTMLProps<HTMLDivElement>) {
-    return <div className={`w-full rounded border border-bg-lighter bg-bg-light p-2 ${props.className ?? ""}`}>
+    return <div className={`w-full rounded border border-bg-lighter bg-bg-light p-2 overflow-y-scroll ${props.className ?? ""}`}>
         {props.children}
     </div>;
 }
