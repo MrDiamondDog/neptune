78e444a7b5b00cb0d32308950e5bdcd8f2e84424
Drew Ratner <>
2026-08-28 10:32:19 -0600

    fix: stupid

--- src/components/misc/SmartOverview.tsx
+++ src/components/misc/SmartOverview.tsx
@@ -18,7 +18,7 @@ export function NodeList({ nodes }: { nodes: React.ReactNode[] }): React.ReactNo
 	if (nodes.length === 1)
 		return nodes[0];
 	if (nodes.length === 2)
-		return <>{nodes[0]} and {nodes[1]}`</>;
+		return <>{nodes[0]} and {nodes[1]}</>;
 
 	let node = <></>;
 	for (let i = 0; i < nodes.length; i++)
