61ac6141386a196b03b96f94ef5b967cf545e266
Drew Ratner <>
2026-09-16 12:53:23 -0600

    fix: checkbox styling

--- src/components/primitives/Checkbox.tsx
+++ src/components/primitives/Checkbox.tsx
@@ -7,7 +7,7 @@ type Props = {
 } & React.PropsWithChildren & React.InputHTMLAttributes<HTMLInputElement>;
 
 export default function Checkbox({ checked, onCheckedChange, children, ...props }: Props) {
-    return <div className={`flex gap-1 items-center cursor-pointer ${props.className ?? ""}`} onClick={() => onCheckedChange(!checked)}>
+    return <div className={`flex gap-2 items-center cursor-pointer ${props.className ?? ""}`} onClick={() => onCheckedChange(!checked)}>
         <div className={`size-5 border-3 bg-bg-lighter border-bg-lighter ${checked && "bg-primary border-primary"}
             transition-colors`}>
             {checked && <Check size={15} />}
